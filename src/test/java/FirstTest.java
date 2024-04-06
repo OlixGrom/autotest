a367e7b5dea4242627502593663223ffaeee6ab0
Olga <kofa_yox@mail.ru>
2024-04-06 22:15:41 +0600

    update

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -2,16 +2,21 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class FirstTest {
     private final static Logger logger = LogManager.getLogger(FirstTest.class);
     private WebDriver webDriver;
+    private static final By test = By.xpath("//button[contains(text(),'Change message')]");
 
     @BeforeAll
     public static void init() {
@@ -20,10 +25,6 @@ public class FirstTest {
         WebDriverManager.chromedriver().setup();
         logger.trace("Скачивание WebDriverManager - завершено");
         logger.trace("======================================================");
-    }
-
-    @BeforeEach
-    public void open() {
     }
 
     @AfterEach
@@ -67,8 +68,8 @@ public class FirstTest {
         WebElement span = webDriver.findElement(new By.ByXPath("//ol[@class='react-results--main']/li[1]//a[@data-testid='result-title-a']//span"));
         String title = span.getText();
         logger.trace("Сравниваем с эталоном");
-        System.out.println("ОР "+text);
-        System.out.println("ФР "+title);
+        System.out.println("ОР " + text);
+        System.out.println("ФР " + title);
         Assertions.assertEquals(text, title);
         logger.trace("======================================================");
     }
@@ -92,18 +93,14 @@ public class FirstTest {
         logger.trace("Открытие сайта картинок- начато");
         webDriver.get("https://demo.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818");
         logger.trace("Открытие сайта - завершено");
-        WebElement pict = webDriver.findElement(new By.ByXPath("//ul[@class='portfolio-area clearfix p-0 m-0']/li[1]"));
+        WebElement pict = webDriver.findElement(new By.ByXPath("//li[@data-id='id-1']"));
         logger.trace("Кликаем на первую картинку");
         pict.click();
-        try {
-            Thread.sleep(3000);
-        } catch (Exception ignored) {
-        }
-        logger.trace("Проверяем модальное окно - открыто? ");
-        WebElement modalForm = webDriver.findElement(new By.ByXPath("//div[@class='pp_overlay']")); //("//a[@class='pp_previous disabled']"));
+        WebElement modalForm = new WebDriverWait(webDriver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(new By.ByXPath("//div[@class='pp_overlay']")));
         System.out.println("ОР true");
-        System.out.println("ФР "+modalForm.isDisplayed());
-        Assertions.assertEquals(true, modalForm.isDisplayed());
+        System.out.println("ФР " + modalForm.isDisplayed());
+        Assertions.assertTrue(modalForm.isDisplayed());
         logger.trace("======================================================");
     }
 
@@ -113,8 +110,10 @@ public class FirstTest {
     Авторизоваться под каким-нибудь тестовым пользователем(можно создать нового)
     Вывести в лог все cookie
     * */
-    @Test
-    public void maximizeMode() {
+    //@Test
+    @ParameterizedTest
+    @CsvSource({"email,password"})
+    public void maximizeMode(String email, String password) {
         logger.trace("======================================================");
         webDriver = new ChromeDriver();
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -126,24 +125,14 @@ public class FirstTest {
         WebElement button = webDriver.findElement(new By.ByXPath(btnEnter));
         button.click();
         logger.trace("Ввод почты");
-        WebElement email = webDriver.findElement(new By.ByXPath("//div[./input[@name='email']]"));
-        email.click();
-        email = webDriver.findElement(new By.ByXPath("//input[@name='email']"));
-        email.sendKeys("otus51423@mailto.plus");
-
+        webDriver.findElement(new By.ByXPath("//div[./input[@name='email']]")).click();
+        webDriver.findElement(new By.ByXPath("//input[@name='email']")).sendKeys(email);
         logger.trace("Ввод пароля");
-        WebElement password = webDriver.findElement(new By.ByXPath("//div[./input[@type='password']]"));
-        password.click();
-        password = webDriver.findElement(new By.ByXPath("//input[@type='password']"));
-        password.sendKeys("12345678Qw1!");
-
+        webDriver.findElement(new By.ByXPath("//div[./input[@type='password']]")).click();
+        webDriver.findElement(new By.ByXPath("//input[@type='password']")).sendKeys(password);
         logger.trace("Клик на Войти");
         WebElement enter = webDriver.findElement(new By.ByXPath("//button[./*[text()='Войти']]"));
         enter.click();
-        try {
-            Thread.sleep(5500);
-        } catch (Exception ignored) {
-        }
         logger.trace("Отобразить куки " + webDriver.manage().getCookies());
         System.out.println(webDriver.manage().getCookies());
         logger.trace("======================================================");
